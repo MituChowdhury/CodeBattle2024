ba3a93694b554f68b47e6d31eca210ce6f9103eb
mdmab <muhtasim.ahmed.bhuiyan@gmail.com>
2024-01-30 19:04:31 +0600

    Updated command placeholders

--- src/main/java/command/BuildCommand.java
+++ src/main/java/command/BuildCommand.java
@@ -4,28 +4,17 @@ import TowerDefense.Attacker;
 import com.codingame.game.Player;
 
 public class BuildCommand extends Command {
-    private Player player;
-//    private int characterId;
-    private Attacker attacker;
     private String objectName;
     private int posX;
     private int posY;
 
     public BuildCommand(Player player, Attacker attacker, String objectName, int posX, int posY) {
-        this.player = player;
-        this.attacker = attacker;
+        super(player, attacker);
         this.objectName = objectName;
         this.posX = posX;
         this.posY = posY;
     }
 
-    public Player getPlayer() {
-        return player;
-    }
-
-    public Attacker getAttacker() {
-        return attacker;
-    }
 
     public String getObjectName() {
         return objectName;
--- src/main/java/command/GoCommand.java
+++ src/main/java/command/GoCommand.java
@@ -4,22 +4,12 @@ import TowerDefense.Attacker;
 import com.codingame.game.Player;
 
 public class GoCommand extends Command {
-    private Player player;
-    private Attacker attacker;
 //    private Attacker attacker;
 
     public GoCommand(Player player, Attacker attacker) {
-        this.player = player;
-        this.attacker = attacker;
+        super(player, attacker);
     }
 
-    public Player getPlayer() {
-        return player;
-    }
-
-    public Attacker getAttacker() {
-        return attacker;
-    }
 
     public String toString() {
         return "Player: " + player.getIndex() + ", " +
