ee6a1f84659b77e692e53ca2afbdd88648cb7ac9
Rabbani2019331039 <fathoor39@student.sust.edu>
2024-01-30 22:05:26 +0600

    stunt effect added

--- src/main/java/com/codingame/game/Referee.java
+++ src/main/java/com/codingame/game/Referee.java
@@ -87,7 +87,7 @@ public class Referee extends AbstractReferee {
 		try {
 			if (turn == 4) {
 //				board.cacheBuild(gameManager.getActivePlayers().get(1), 15, 7, "GUNTOWER");
-				board.cacheBuild(gameManager.getActivePlayers().get(0), 9, 7, "GUNTOWER");
+				board.cacheBuild(gameManager.getActivePlayers().get(0), 9, 7, "GLUETOWER");
 			}
 
 			if (turn == 12) {
--- src/main/java/view/GlueTowerView.java
+++ src/main/java/view/GlueTowerView.java
@@ -1,7 +1,10 @@
 package view;
 
+import TowerDefense.Constants;
+import com.codingame.gameengine.module.entities.Curve;
 import com.codingame.gameengine.module.entities.GraphicEntityModule;
 import com.codingame.gameengine.module.entities.Group;
+import com.codingame.gameengine.module.entities.SpriteAnimation;
 import com.codingame.gameengine.module.tooltip.TooltipModule;
 
 import TowerDefense.Attacker;
@@ -9,24 +12,52 @@ import TowerDefense.Tower;
 
 public class GlueTowerView extends TowerView {
 
+
+	private SpriteAnimation stuntAnimation;
+
 	public GlueTowerView(Tower tower, Group boardGroup, GraphicEntityModule graphics, TooltipModule tooltips) {
 		super(tower, boardGroup, graphics, tooltips, "glueTower");
-		attackLine = graphics.createLine();
-		attackLine.setX((int) (BoardView.CELL_SIZE * (tower.getTile().getX() + 0.5)));
-		attackLine.setY(BoardView.CELL_SIZE * tower.getTile().getY());
-		attackLine.setLineColor(0xff0000).setAlpha(0);
-		attackLine.setLineWidth(5);
+
+
+
+		String[] stuntSprite = graphics.createSpriteSheetSplitter()
+				.setSourceImage("ground_effect.png")
+				.setHeight(64).setWidth(64).setImageCount(8)
+				.setImagesPerRow(8).setOrigRow(0).setOrigCol(0).setName("stuntEffectSprite").split();
+
+
+		double gg = (double) graphics.getWorld().getHeight() / (Constants.MAP_HEIGHT * 100);
+
+		stuntAnimation = graphics.createSpriteAnimation()
+				.setImages(stuntSprite)
+				.setScaleX(5)
+				.setScaleY(12)
+				.setDuration(500)
+				.setLoop(true).setPlaying(true)
+				.setX((int) (BoardView.CELL_SIZE * (tower.getTile().getX() + 0.5) * gg) + Constants.BOARD_DASH_WIDTH)
+				.setY((int) (BoardView.CELL_SIZE * (tower.getTile().getY() + 0.5) * gg))
+				.setAnchor(.5)
+				.setVisible(false);
+
+
+
 		commitSprites();
 		updateTooltip();
 	}
 
 	@Override
 	public void attack(Attacker a) {
-		attackLine.setAlpha(1);
-		attackLine.setX2((int) (BoardView.CELL_SIZE * (a.getLocation().getX() + 0.5)));
-		attackLine.setY2((int) (BoardView.CELL_SIZE * a.getLocation().getY()));
 
-		graphics.commitEntityState(0, attackLine);
-		attackLine.setAlpha(0);
+
+		stuntAnimation
+				.setVisible(true)
+				.setAlpha(1);
+
+		graphics.commitEntityState(1, stuntAnimation);
+
+		stuntAnimation
+				.setAlpha(0, Curve.LINEAR)
+				.setVisible(false);
+
 	}
 }
