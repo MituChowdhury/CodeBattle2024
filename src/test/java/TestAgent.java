793297134ed0afa352e2ea6204b6edf03069019e
Imran <imranbinazad777@gmail.com>
2024-02-04 14:18:04 +0600

    html page updated

--- src/test/java/TestAgent.java
+++ src/test/java/TestAgent.java
@@ -1,157 +1,169 @@
-import java.lang.reflect.Array;
-import java.util.*;
-import java.io.*;
-import java.math.*;
+import java.util.Scanner;
 
-/**
- * Survive the attack waves
- **/
-class TestAgent {
+public class TestAgent {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int playerID, w, h;
 
+        // initial input
+        playerID = 0;//sc.nextInt();
 
-    public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
-//        int playerId = in.nextInt();
-        // Initial inputs...
-        int side = 0;
-        int width = in.nextInt();
-        int height = in.nextInt();
+        // map width and height
+        w = sc.nextInt();
+        h = sc.nextInt();
+        System.err.println("width height done");
 
-        System.err.printf("playerId: %d, width: %d, height: %d\n", 0, width, height);
-
-
-        if (in.hasNextLine()) {
-            in.nextLine();
+        if (sc.hasNextLine()) {
+            sc.nextLine();
         }
 
-        for (int i = 0; i < height; i++) {
-            String line = in.nextLine();
-            System.err.printf("line: %s\n", line);
+        // map inputs
+        String[] arr = new String[h];
+
+        for (int i = 0; i < h; i++) {
+                arr[i] = sc.next();
         }
 
-        String yCoords = "0 2 4 12 13";
+        // starting position of my pawns
+        int start = 0;
+        for (int i = 0; i < 5; i++) {
+            System.out.println(start);
+            start = start + (h / 5);
+        }
 
-        System.out.println(0);
-        System.out.println(2);
-        System.out.println(4);
-        System.out.println(8);
-        System.out.println(9);
 
-        int cnt =0;
-        // game loop
+        int myMoney, oppMoney, scorePlayer, scoreOpp;
+
+        int[][] myplayers = new int[5][5];
+        int[][] oPlayers = new int[5][5];
+
+        int turn = 0;
+        // game turn
         while (true) {
-            // Money....
-            int myMoney = in.nextInt();
-            int opponentMoney = in.nextInt();
 
-            // Scores....
-            int myScore = in.nextInt();
-            int opponentScore = in.nextInt();
+            // Money
+            myMoney = sc.nextInt();
+            oppMoney = sc.nextInt();
 
-            System.err.printf("Money: %d, Score: %d, OpponentMoney: %d, OpponentScore: %d\n", myMoney, myScore, opponentMoney, opponentScore);
+            // score
+            scorePlayer = sc.nextInt();
+            scoreOpp = sc.nextInt();
 
-            // Status of the player's characters....
+            if (sc.hasNextLine()) {
+                sc.nextLine();
+            }
+            if (turn != 0) {
+                // player 0 possitions
+                for (int k = 0; k < 5; k++) {
+                    for (int l = 0; l < 5; l++) {
+                        myplayers[k][l] = sc.nextInt();
+                    }
+                }
 
-            if (in.hasNextLine()) {
-                in.nextLine();
+                // player 1 positions
+                for (int k = 0; k < 5; k++) {
+                    for (int l = 0; l < 5; l++) {
+                        oPlayers[k][l] = sc.nextInt();
+                    }
+                }
+            }
+                // obstacle numbers input
+                int n_t = sc.nextInt();
+
+                if (sc.hasNextLine()) {
+                    sc.nextLine();
+                }
+
+
+            // obstacle array
+            int[][] obs = new int[n_t][2];
+
+            for (int m = 0; m < n_t; m++) {
+                String type = sc.next();
+                int id = sc.nextInt();
+                int owner = sc.nextInt();
+                int posX = sc.nextInt();
+                int posY = sc.nextInt();
+                int health = sc.nextInt();
+                int damage = sc.nextInt();
+                int range = sc.nextInt();
+                int cooldown = sc.nextInt();
+                int bounty = sc.nextInt();
+
+                if (owner != playerID) {
+                    obs[m][0] = posX;
+                    obs[m][1] = posY;
+                } else {
+                    obs[m][0] = -1;
+                    obs[m][1] = -1;
+                }
             }
 
-            int offset = 5;
-            if (cnt != 0) {
-                // At first, there is neither attackers nor veterans....
-                System.err.println("Player characters: ");
+            // players logic to print outputs
+            if(turn!=0) {
 
-                for (int i = 0; i < 5; ++i) {
-                    int playerCharId = in.nextInt();
-                    int playerPosX = in.nextInt();
-                    int playerPosY = in.nextInt();
-                    int playerHealth = in.nextInt();
-                    int playerSpeed = in.nextInt();
+                for (int p = 0; p < 5; p++) {
 
+                    // if no position given skip this turn
+                    if (myplayers[p][0] == -1)
+                        continue;
 
-                    if( i == 10000 ) {
-                        if( cnt == offset ) {
-                            System.out.println("build " + playerCharId + " SPRING_NORTH " + playerPosX + " " + (int)(playerPosY));
-//                            System.out.println("attack " + playerCharId + " NORTH");
-                        } else if( cnt == offset+6) {
-                            System.out.println("build " + playerCharId + " GUN_TOWER " + playerPosX + " " + (playerPosY));
-                        } else if( cnt == offset+9) {
-                            System.out.println("build " + playerCharId + " SPRING_NORTH " + playerPosX + " " + (playerPosY));
-                        } else if( cnt == offset+14) {
-                            System.out.println("build " + playerCharId + " WALL " + (playerPosX) + " " + playerPosY);
-                        } else {
-                            System.out.println("go " + playerCharId);
-                        }
-
-                    } else {
-                        System.out.println("go " + playerCharId);// + " " + side + " " + i);
+                    // build some objects early
+                    if (turn <= 10) {
+                        if (turn % 2 == 0 && myMoney >= 400)
+                            System.out.println("build p GUN_TOWER " + myplayers[p][1] + " " + myplayers[p][2]);
+                        else
+                            System.out.println("go " + myplayers[p][0]);
                     }
 
-                    System.err.printf("\tId: %d, Pos_X: %d, Pos_Y: %d, Health: %d, Speed: %d\n", playerCharId, playerPosX, playerPosY, playerHealth, playerSpeed);
+                    // then only march forward or attack
+                    else {
+                        switch (isPresent(myplayers[p][1], myplayers[p][2], obs, n_t)) {
+                            case 21:
+                                System.out.println("attack " + myplayers[p][0] + " west");
 
-//                    String line = in.nextLine();
+                                break;
+                            case 23:
+                                System.out.println("attack " + myplayers[p][0] + " east");
+
+                                break;
+                            case 12:
+                                System.out.println("attack " + myplayers[p][0] + " north");
+                                break;
+
+                            case 32:
+                                System.out.println("attack " + myplayers[p][0] + " south");
+                                break;
+
+                            default:
+                                System.out.println("go " + myplayers[p][0]);
+                                break;
+                        }
+                    }
+
+
                 }
-
-                // Status of the opponent's characters....
-                System.err.println();
-                System.err.println("Opponent characters: ");
-
-                for (int i = 0; i < 5; ++i) {
-                    int opponentCharId = in.nextInt();
-                    int opponentPosX = in.nextInt();
-                    int opponentPosY = in.nextInt();
-                    int opponentHealth = in.nextInt();
-                    int opponentSpeed = in.nextInt();
-
-                    System.err.printf("\tId: %d, Pos_X: %d, Pos_Y: %d, Health: %d, Speed: %d\n", opponentCharId, opponentPosX, opponentPosY, opponentHealth, opponentSpeed);
-
-//                    String line = in.nextLine();
-                }
             }
 
-            // Tower count...
-//            int towerCount = in.nextInt();
-            // Object count...
-
-//            if (in.hasNextLine())
-//                in.nextLine();
-
-//            for (int i = 0; i < towerCount; i++) {
-//                String towerAll = in.nextLine();
-//                System.err.println(towerAll);
-//            }
-
-            int objCount = in.nextInt();
-
-            if (in.hasNextLine()) {
-                in.nextLine();
-            }
-
-            for (int i = 0; i < objCount; ++i) {
-                String type = in.next();
-                int id = in.nextInt();
-                int owner = in.nextInt();
-                int posX = in.nextInt();
-                int posY = in.nextInt();
-                int health = in.nextInt();
-                int damage = in.nextInt();
-                int range = in.nextInt();
-                int cooldown = in.nextInt();
-                int bounty = in.nextInt();
-
-//                String line = in.nextLine();
-            }
-
-
-//            System.out.println("go 0");
-//            System.out.println("go 1");
-//            System.out.println("go 2");
-//            System.out.println("go 3");
-//            System.out.println("go 4");
-
-//            System.err.println("Debug messages... here "+cnt);
-            cnt++;
+            turn++;
             System.out.flush();
+
         }
+
+    }
+
+    static int isPresent(int x1, int y1, int[][] obs, int size) {
+
+        for (int i = 0; i < size; i++) {
+            if (obs[i][0] == x1 && obs[i][1] == y1 + 1)
+                return 23;
+            else if (obs[i][0] == x1 && obs[i][1] == y1 - 1)
+                return 21;
+            else if (obs[i][0] == x1 + 1 && obs[i][1] == y1)
+                return 32;
+            else if (obs[i][0] == x1 - 1 && obs[i][1] == y1)
+                return 12;
+        }
+        return 0;
     }
 }
